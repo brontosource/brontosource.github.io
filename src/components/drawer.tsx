f76b96b6cee9b15b24f648e4a0275f4901029463
Elad <elad.karni@gmail.com>
2024-10-06 21:00:27 -0400

    Fix: Removed Nav and leftover elements associated with

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import MobileNavigation from "@/components/navbar/mobilenav";
-import Navbar from "@/components/navbar/navbar";
 import Footer from "@/components/footer";
 
 type DrawerProps = {
@@ -11,7 +9,6 @@ const Drawer = ({ children }: DrawerProps) => {
   return (
     <div className="drawer-end min-h-screen flex flex-col">
       <div className="drawer-content flex flex-col grow justify-between">
-        {/* <Navbar /> */}
         {children}
         <Footer />
       </div>
