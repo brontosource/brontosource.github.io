c64d7aa6b6105a35f6bed244bda6debcee78b3fa
Elad Karni <elad.karni@gmail.com>
2024-10-12 20:53:07 -0400

    Chor: Prettier Formatting Added for code readability

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,17 +1,17 @@
 export const navLinkList = [
-    {
-        label: "Home",
-        style: "default",
-        url: "/",
-    },
-    {
-        label: "About",
-        style: "default",
-        url: "#about",
-    },
-    {
-        label: "Contact",
-        style: "default",
-        url: "#contact",
-    },
+  {
+    label: "Home",
+    style: "default",
+    url: "/",
+  },
+  {
+    label: "About",
+    style: "default",
+    url: "#about",
+  },
+  {
+    label: "Contact",
+    style: "default",
+    url: "#contact",
+  },
 ];
--- src/content/FeatureCompare.tsx
+++ src/content/FeatureCompare.tsx
@@ -1,45 +1,75 @@
-import Image from 'next/image'
-import CharletteStoneTablet from '../../public/Charlette-StoneTablet.webp'
-import SectionContainer from '@/ui/SectionContainer'
-import TitleText from '@/ui/TitleText'
+import Image from "next/image";
+import CharletteStoneTablet from "../../public/Charlette-StoneTablet.webp";
+import SectionContainer from "@/ui/SectionContainer";
+import TitleText from "@/ui/TitleText";
 
 const FeatureCompare = () => {
-    return (
-        <SectionContainer sectionName="feature-compare" sectionClasses="flex flex-col gap-8 justify-between">
-            <span>
-                <TitleText>
-                    Our Solution
-                </TitleText>
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim repellendus molestias expedita obcaecati, esse voluptatibus in similique beatae saepe voluptas autem iste officiis explicabo aliquam, voluptate aliquid pariatur!</h3>
-            </span>
-            <div className='flex md:flex-row flex-col justify-between'>
-                <Image src={CharletteStoneTablet} alt="Charlette the BrontoSource reading a stone tablet" style={{ objectFit: 'cover', width: 'auto', height: '100%' }} className='md:block hidden lg:max-h-[32rem] md:max-h-96' />
-                <div className='flex flex-col justify-between gap-'>
-                    <div className='flex flex-col-reverse justify-center'>
-                        <h2>Input C++</h2>
-                        <div className="mockup-code text-base-100">
-                            <pre><code>{`float deref(float *f) {`}</code></pre>
-                            <pre><code>{`    return *f;`}</code></pre>
-                            <pre><code>{`}`}</code></pre>
-                        </div>
-                    </div>
-                    <Image src={CharletteStoneTablet} alt="Charlette the BrontoSource reading a stone tablet" style={{ objectFit: 'contain', width: 'auto', height: '100%', maxHeight: '384px' }} className='block md:hidden' />
-                    <div className='flex flex-col md:flex-col-reverse justify-center'>
-                        <h2>BrontoSource Output</h2>
-                        <div className="mockup-code bg-primary text-primary-content">
-                            <pre><code>{`fn deref(f: &mut f32) -> f32 {`}</code></pre>
-                            <pre><code>{`    *f`}</code></pre>
-                            <pre><code>{`}`}</code></pre>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <SectionContainer
+      sectionName="feature-compare"
+      sectionClasses="flex flex-col gap-8 justify-between"
+    >
+      <span>
+        <TitleText>Our Solution</TitleText>
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+          repellendus molestias expedita obcaecati, esse voluptatibus in
+          similique beatae saepe voluptas autem iste officiis explicabo aliquam,
+          voluptate aliquid pariatur!
+        </h3>
+      </span>
+      <div className="flex md:flex-row flex-col justify-between">
+        <Image
+          src={CharletteStoneTablet}
+          alt="Charlette the BrontoSource reading a stone tablet"
+          style={{ objectFit: "cover", width: "auto", height: "100%" }}
+          className="md:block hidden lg:max-h-[32rem] md:max-h-96"
+        />
+        <div className="flex flex-col justify-between gap-">
+          <div className="flex flex-col-reverse justify-center">
+            <h2>Input C++</h2>
+            <div className="mockup-code text-base-100">
+              <pre>
+                <code>{`float deref(float *f) {`}</code>
+              </pre>
+              <pre>
+                <code>{`    return *f;`}</code>
+              </pre>
+              <pre>
+                <code>{`}`}</code>
+              </pre>
             </div>
-
-
-        </SectionContainer>
-    )
-}
-
+          </div>
+          <Image
+            src={CharletteStoneTablet}
+            alt="Charlette the BrontoSource reading a stone tablet"
+            style={{
+              objectFit: "contain",
+              width: "auto",
+              height: "100%",
+              maxHeight: "384px",
+            }}
+            className="block md:hidden"
+          />
+          <div className="flex flex-col md:flex-col-reverse justify-center">
+            <h2>BrontoSource Output</h2>
+            <div className="mockup-code bg-primary text-primary-content">
+              <pre>
+                <code>{`fn deref(f: &mut f32) -> f32 {`}</code>
+              </pre>
+              <pre>
+                <code>{`    *f`}</code>
+              </pre>
+              <pre>
+                <code>{`}`}</code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+    </SectionContainer>
+  );
+};
 
 // #![allow(dead_code, mutable_transmutes, non_camel_case_types, non_snake_case, non_upper_case_globals, unused_assignments, unused_mut)]
 // #[no_mangle]
@@ -47,4 +77,4 @@ const FeatureCompare = () => {
 //     return *f;
 // }
 
-export default FeatureCompare
+export default FeatureCompare;
