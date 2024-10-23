df8be8680b91e3ff5c3c8934352ceb3ba3479e4a
Elad Karni <elad.karni@gmail.com>
2024-10-23 14:34:43 -0400

    Fix: Changed KeyFeatures cards to be more uniform

--- src/components/FeatCard.tsx
+++ src/components/FeatCard.tsx
@@ -12,30 +12,26 @@ type FeatCardProps = {
 
 const FeatCard = ({ image, title, content, showButton }: FeatCardProps) => {
     return (
-        <article className="bg-base-100 h-fit shadow-xl">
-            <figure className="bg-base-200 pl-2 pt-2 relative">
-                <Image
-                    src={image.data}
-                    alt={image.alt}
-                    style={{
-                        width: "auto",
-                    }}
-                    className="bottom-0 left-0 pl-2"
-                />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title text-primary">
-                    {title}
-                </h2>
-                <p className="text-primary-content/80">
-                    {content}
-                </p>
-                {showButton && (
-                    <button className="btn btn-primary ">Learn More</button>
-                )}
-            </div>
-        </article>
-    )
+      <article className="bg-base-100 h-fit shadow-xl min-h-[455px] flex flex-col">
+        <figure className="bg-base-200 pl-2 pt-2 relative">
+          <Image
+            src={image.data}
+            alt={image.alt}
+            style={{
+              width: "auto",
+            }}
+            className="bottom-0 left-0 pl-2"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-primary">{title}</h2>
+          <p className="text-primary-content/80 h-fit">{content}</p>
+          {showButton && (
+            <button className="btn btn-primary ">Learn More</button>
+          )}
+        </div>
+      </article>
+    );
 }
 
 export default FeatCard
