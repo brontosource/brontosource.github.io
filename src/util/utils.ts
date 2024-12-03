2b9361ddf967a32e5eaba0ea3343d4449150e959
Elad Karni <elad.karni@gmail.com>
2024-12-03 16:22:38 -0500

    Chor: Code Clean-Up

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,6 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { createClient } from "contentful";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
